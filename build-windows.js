52a52dbf43f3fb1cc536cc116e721d951b02edbf
jnplonte <jnpl.onte@gmail.com>
2017-09-15 17:35:22 +0800

    first commit

---
+++ build-windows.js
@@ -0,0 +1,26 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+     .then(createWindowsInstaller)
+     .catch((error) => {
+     console.error(error.message || error)
+     process.exit(1)
+ })
+
+function getInstallerConfig () {
+    console.log('creating windows installer')
+    const rootPath = path.join('./')
+    const outPath = path.join(rootPath, 'release-builds')
+
+    return Promise.resolve({
+       appDirectory: path.join(outPath, 'Otomeyt-win32-ia32'),
+       authors: 'John Paul Onte',
+       noMsi: true,
+       outputDirectory: path.join(outPath, 'installers', 'win'),
+       exe: 'otomeyt.exe',
+       setupExe: 'otomeytInstaller.exe',
+       description: 'otomeyt desktop app',
+       setupIcon: path.join(rootPath, 'src', 'img', 'otomeyt.ico')
+   })
+}
